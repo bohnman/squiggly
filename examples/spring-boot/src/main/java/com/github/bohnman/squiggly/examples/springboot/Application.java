d4968459311efaaff2ba581a950336a4c9f2cadc
Ryan Bohn <rbohn@biohitech.com>
2017-10-05 10:38:49 -0400

    updating spring boot example to include setting object mapper for MappingJackson2HttpMessageConverter

--- examples/spring-boot/src/main/java/com/github/bohnman/squiggly/examples/springboot/Application.java
+++ examples/spring-boot/src/main/java/com/github/bohnman/squiggly/examples/springboot/Application.java
@@ -6,10 +6,13 @@ import com.github.bohnman.squiggly.Squiggly;
 import com.github.bohnman.squiggly.examples.springboot.web.ListResponse;
 import com.github.bohnman.squiggly.web.RequestSquigglyContextProvider;
 import com.github.bohnman.squiggly.web.SquigglyRequestFilter;
+import com.google.common.collect.Iterables;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -25,8 +28,9 @@ public class Application {
     }
 
     public static void main(String[] args) throws Exception {
-        Iterable<ObjectMapper> objectMappers = SpringApplication.run(Application.class, args)
-            .getBeansOfType(ObjectMapper.class)
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        Iterable<ObjectMapper> objectMappers = context.getBeansOfType(ObjectMapper.class)
             .values();
 
         Squiggly.init(objectMappers, new RequestSquigglyContextProvider() {
@@ -41,6 +45,15 @@ public class Application {
                 return filter;
             }
         });
+
+        ObjectMapper objectMapper = Iterables.getFirst(objectMappers, null);
+
+        // Enable Squiggly for Jackson message converter
+        if (objectMapper != null) {
+            for (MappingJackson2HttpMessageConverter converter : context.getBeansOfType(MappingJackson2HttpMessageConverter.class).values()) {
+                converter.setObjectMapper(objectMapper);
+            }
+        }
     }
 
 }
