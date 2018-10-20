196b472e772ea989e5725339d859313d2e5f93d7
Ryan Bohn <rbohn@biohitech.com>
2018-10-20 19:54:13 -0400

    fixing response thread local

--- squiggly-core/src/main/java/com/github/bohnman/squiggly/core/web/SquigglyRequestFilter.java
+++ squiggly-core/src/main/java/com/github/bohnman/squiggly/core/web/SquigglyRequestFilter.java
@@ -5,6 +5,7 @@ import com.github.bohnman.squiggly.core.variable.SquigglyVariablesHolder;
 import javax.annotation.concurrent.ThreadSafe;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -31,6 +32,7 @@ public class SquigglyRequestFilter implements Filter {
 
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         SquigglyRequestHolder.setRequest(httpRequest);
+        SquigglyResponseHolder.setResponse((HttpServletResponse) response);
         SquigglyVariablesHolder.set(toVariables(httpRequest));
 
         try {
