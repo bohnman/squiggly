8efa82383dbd1bb60b247e4f75c975768ff93862
Ryan Bohn <rbohn@biohitech.com>
2019-05-29 10:36:46 -0400

    adding a thread local context provider

---
+++ src/main/java/com/github/bohnman/squiggly/context/provider/ThreadLocalContextProvider.java
@@ -0,0 +1,9 @@
+package com.github.bohnman.squiggly.context.provider;
+
+public class ThreadLocalContextProvider extends AbstractSquigglyContextProvider {
+
+    @Override
+    protected String getFilter(Class beanClass) {
+        return SquigglyFilterHolder.getFilter();
+    }
+}
